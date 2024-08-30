8237926b35e548ff784bca7271a328ccec36a59b
Jorge Machado <jrms_jorgesilva@hotmail.com>
2024-08-30 16:02:35 -0300

    feat: adicionados compoenents e handlers para functions de manipulação CRUD

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from "react";
+import { zonasApiMock } from "../mock/zonas";
 
 const Add = () => {
-  return (
-    <div>Add</div>
-  )
-}
+  const [zona, setZona] = useState<any>(() => zonasApiMock);
 
-export default Add
+  const addZona = async () => {};
+
+  const handleZona = async (e: any) => {
+    setZona((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+    console.log("zona: ", zona);
+  };
+
+  return (
+    <>
+      <h1>Adicionar zona eleitoral</h1>
+      <label>Numero</label>
+      <input
+        type="number"
+        name="numero"
+        placeholder="Numero"
+        onChange={handleZona}
+      />
+      <label>Endereço</label>
+      <input
+        type="text"
+        name="endereco"
+        placeholder="endereço"
+        onChange={handleZona}
+      />
+      <label>Bairro</label>
+      <input
+        type="text"
+        name="bairro"
+        placeholder="bairro"
+        onChange={handleZona}
+      />
+      <label>Cidade</label>
+      <input
+        type="text"
+        name="cidade"
+        placeholder="cidade"
+        onChange={handleZona}
+      />
+      <label>Email</label>
+      <input
+        type="text"
+        name="email"
+        placeholder="email"
+        onChange={handleZona}
+      />
+      <button className="btn-add" onClick={addZona}>
+        Adicionar Zona
+      </button>
+    </>
+  );
+};
+
+export default Add;
