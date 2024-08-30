a9ec9f952a8f8f6336e4bdfd6f6c38779956e228
Jorge Machado <jrms_jorgesilva@hotmail.com>
2024-08-30 13:24:29 -0300

    feat: adicionados mackups para API TSE e Home componentes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { zonasApiMock } from "../mock/zonas";
 import { locaisVotacaoApiMock } from "../mock/locaisVotacao";
 
@@ -7,6 +7,11 @@ const endPointLocaisVotacao = locaisVotacaoApiMock;
 const endPointZonas = zonasApiMock;
 
 const Home = () => {
+  const [zonas, setZonas] = useState<any>(() => zonasApiMock);
+  const [locaisVotacao, setLocaisVotacao] = useState<any>(
+    () => locaisVotacaoApiMock
+  );
+
   useEffect(() => {
     const fetchLocaisVotacao = async () => {
       try {
@@ -23,6 +28,28 @@ const Home = () => {
   return (
     <>
       <h1>TSE App | Eleições Municipais 2024</h1>
+      <h2>Zonas Eleitorais</h2>
+      {zonas?.map((elem: any) => (
+        <>
+          <h5>Número: {elem.numero}</h5>
+          <h5>Endereço: {elem.endereco}</h5>
+          <h5>Bairro: {elem.bairro}</h5>
+          <h5>Cidade: {elem.cidade}</h5>
+          <h5>Email: {elem.email}</h5>
+          <br />
+        </>
+      ))}
+      <h2>Locais de Votação</h2>
+      {locaisVotacao?.map((elem) => (
+        <>
+          <h5>Número: {elem.numero}</h5>
+          <h5>Endereço: {elem.endereco}</h5>
+          <h5>Bairro: {elem.bairro}</h5>
+          <h5>Cidade: {elem.cidade}</h5>
+          <h5>Email: {elem.email}</h5>
+          <br />
+        </>
+      ))}
     </>
   );
 };
